5910808b5509edc1d6caa9c98c3f8befc2785a1c
Ivy Rose <hi@ivyrose.me>
2024-05-29 19:03:04 -0400

    Created serverless function for proxying API calls

---
+++ functions/proxy.mjs
@@ -0,0 +1,16 @@
+import { endpoints } from "../src/endpoints.js";
+
+export default async req => {
+	const params = Object.fromEntries(
+		(new URL(req.url)).searchParams.entries()
+	);
+	let { agency, routeIds } = params;
+
+	const endpoint = endpoints[agency];
+	if (!endpoint?.proxy) {
+		return new Response({ status: 400 });
+	}
+
+	const response = await fetch(endpoint.url(routeIds));
+	return response;
+};
